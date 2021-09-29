3c551b065f058f3bb55ffdb76c8db2425af25385
lanemacdougall <lane.macdougall@ttu.edu>
2021-09-29 16:39:49 -0500

    Add functionality for prompting user for file path of smart contract and retrieving, reading, and parsing the content of specified smart contract

--- smart-vds.js
+++ smart-vds.js
@@ -1,11 +1,48 @@
-/*
-    Simple hello world to run node.js server
- */
-var http = require('http');
-//create a server object:
-http.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response
-    res.end(); //end the response
-}).listen(3000, function(){
-    console.log("server start at port 3000"); //the server object listens on port 3000
-});
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const parser = require('@solidity-parser/parser');
+
+const fileUtils = require('./utils/fileUtils');
+
+// Clear the console window
+clear();
+
+// Log utility header to the console
+console.log(
+    chalk.greenBright(figlet.textSync('Smart VDS', { font: 'cyberlarge', horizontalLayout: 'full' }))
+);
+console.log(chalk.greenBright('A vulnerability detection scanner for smart contracts\n\n'));
+
+/* scan function
+*       Purpose: To retrieve specified Solidity (*.sol) file and detect vulnerabilities within the file's source code
+*       Tasks Executed By Function:
+*           - Prompt user for file path of Solidity file to scan
+*           - Retrieve specified file and read its contents
+*           - Parse Solidity source code into a parse tree
+*           - Retrieve vulnerability patterns
+*           - Detect vulnerability patterns within source code
+*           - Generate report of detected vulnerabilities
+* */
+const scan = async () => {
+    try {
+        // Prompt user for file path and validate input (must be a valid Solidity (*.sol) file)
+        const promptAnswers = await fileUtils.promptForFilePath();
+        const filePath = promptAnswers.filePath;
+
+        // Read contents of specified file
+        const fileContents = fileUtils.readFileContents(filePath).toString();
+
+        // Generate parse tree from retrieved file contents
+        console.log('Parsing Solidity source code...');
+        const parseTree = parser.parse(fileContents);
+        console.log(parseTree);
+    } catch (err) {
+        if (err instanceof parser.ParserError) {
+            console.error(chalk.red(err.errors));
+        }
+        console.log(chalk.red(err));
+    }
+}
+
+scan();
