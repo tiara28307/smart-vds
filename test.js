9df0c030d4fe92daaf9cf20234e2a78d1204a796
tiara28307 <tiara.carroll@ttu.edu>
2021-10-19 12:10:31 -0500

    implemenet function for unchecked call return value vulnerability

--- test.js
+++ test.js
@@ -1,50 +1,41 @@
 const assert = require('assert')
 const db = require('./utils/databaseUtils')
-const { JSONPath } = require('jsonpath-plus')
 const file = require('./utils/fileUtils')
 const parser = require('@solidity-parser/parser')
+const vulnerability = require('./utils/vulnerabilityDetectors')
 
 afterAll(async () => await db.closeDbConnection())
 
 // Test MongoDB Cloud Database
 describe('Test Cloud Database', () => {
   it('DB Connection', async () => {
+    // Arrange
     let isConnected
+
+    // Act
     try {
       isConnected = await db.establishDbConnection()
     } catch (err) {
       assert(err.message).toEqual('DB connection error')
     }
+
+    // Assert
     assert.equal(isConnected, true)
   })
 })
 
-/* Unchecked Call Return Value Vulnerability Detection
-*     Checks for the following:
-*       - Are there low-level calls: call, send, delegatecall, or callcode
-*       - If they exist, are they not within one of the following:
-*           - IfStatement, ReturnStatement, valid expression, FunctionCall, or VariableDeclaration
-* */
 describe('Test Unchecked Call Return Value Vulnerability', () => {
   it('Unchecked Call Return Value Detected', () => {
     // Get parse tree of sample smart contract for Unchecked Call Return Value
+    // Arrange
     const uncheckedCallReturnFilePath = './resources/UNCHECKED_CALL_RETURN_VALUE.sol'
     const fileContents = file.readFileContents(uncheckedCallReturnFilePath).toString()
     const parseTree = parser.parse(fileContents)
 
-    // Vulnerability patterns for Unchecked Call Return Value
-    const p1 = '$..statements[?(@.type != "IfStatement" && @.type != "ReturnStatement" && ' +
-        '@.type != "VariableDeclarationStatement")]'
-    const p2 = '$..expression[?(@.type != "FunctionCall" && (@.memberName == "call" || @.memberName == "send" || ' +
-        '@.memberName == "delegatecall" || @.memberName == "callcode"))]'
+    // Act
+    const vulnerabilityDetected = vulnerability.detectUncheckedCallReturnValue(parseTree)
 
-    // JSON Queries for pattern matching
-    const q1 = JSONPath({ json: parseTree, path: p1, resultType: 'value' })
-    const q2 = JSONPath({ json: q1, path: p2, resultType: 'value' })
-
-    // Check if vulnerability is found
-    const vulnerabilityFound = q2.length > 0
-
-    assert.equal(vulnerabilityFound, true, 'Should return vulnerability found.')
+    // Assert
+    assert.equal(vulnerabilityDetected, true, 'Should return vulnerability found.')
   })
 })
