e90914de18c6f55e69eab7b049014621a11300b4
tiara28307 <tiara.carroll@ttu.edu>
2021-10-29 18:11:07 -0500

    test file updates for overflow and underflow to follow all test cases

--- tests/resources/Underflow/underflow13.sol
+++ tests/resources/Underflow/underflow13.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.5;
 contract underflow
 {
  uint public balance = 0;
- function add(uint256 deposit) public
+ function sub(uint256 deposit) public
  {
   balance = balance -= deposit;
  }
