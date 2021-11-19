58e22f7c72095c7fa7a6bdf78f105c8b48448f2f
tiara28307 <tiara.carroll@ttu.edu>
2021-11-19 09:20:05 -0600

    update unit test files for multiple test suites - improve runtime

---
+++ tests/database.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const db = require('../utils/databaseUtils');
+
+afterAll(async () => await db.closeDbConnection())
+
+describe('Test Cloud Database', () => {
+  it('DB Connection', async () => {
+    // Arrange
+    let isConnected
+
+    // Act
+    try {
+      isConnected = await db.establishDbConnection()
+    } catch (err) {
+      assert(err.message).toEqual('DB connection error')
+    }
+
+    // Assert
+    assert.equal(isConnected, true)
+  })
+})
