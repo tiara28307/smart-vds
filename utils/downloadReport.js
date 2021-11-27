0dbc7010a409c1888dc0a397f9adedee4e2e0ad3
tiara28307 <tiara.carroll@ttu.edu>
2021-11-26 18:04:32 -0600

    download report function

---
+++ utils/downloadReport.js
@@ -0,0 +1,159 @@
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const PDFDocument = require('pdfkit-table')
+
+const doc = new PDFDocument({ size: 'A4' })
+const vulnInfo = require('./vulnerabilityInfo')
+
+/* downloadReport function
+*       Purpose: To generate pdf version of Smart VDS Report for user to download
+*       Tasks Executed By Function:
+*           - Create pdf document with vulnerabilities detected information
+*           - Return file path to downloaded report
+* */
+const downloadReport = async () => {
+  const vulnerabilities = vulnInfo.getVulnInformation()
+  const totalVulnerabilityCount = vulnInfo.getTotalVulnCount()
+
+  // Get file path to user's `Download` directory
+  const filePath = path.join(os.homedir(), 'Downloads')
+
+  // Write to file path for Smart VDS Report
+  doc
+    .pipe(fs.createWriteStream(`${filePath}/smart-vds-report.pdf`))
+
+  // Create pdf document
+  doc
+    .font('Times-Roman')
+    .fontSize('20')
+    .text('Smart VDS Report', {
+      align: 'left'
+    })
+    .fontSize('10')
+    .moveDown(2)
+    .text(new Date().toUTCString(), {
+      align: 'left'
+    })
+    .font('Times-Bold')
+    .moveDown(0.5)
+    .text(`Number of Vulnerabilities Found: ${totalVulnerabilityCount}`, {
+      align: 'left'
+    })
+    .moveDown(2)
+    .fontSize('12')
+    .text('Overview', {
+      align: 'left'
+    })
+    .moveDown(1)
+
+  // Generate table for report overview
+  const table = {
+    title: '',
+    fontSize: 10,
+    headers: [
+      { label: 'Name', property: 'name', width: 150, headerAlign: 'center', renderer: null },
+      { label: 'Severity', property: 'severity', width: 90, align: 'center', renderer: null },
+      { label: 'Number Found', property: 'count', width: 90, align: 'center', renderer: null }
+    ],
+    datas: vulnerabilities
+  }
+
+  const tableOptions = {
+    prepareHeader: () => doc.font('Times-Bold').fontSize(10),
+    prepareRow: (row, indexColumn, indexRow, rectRow) => doc.font('Times-Roman').fontSize(10)
+  }
+
+  await doc.table(table, tableOptions, () => {})
+
+  doc
+    .moveDown(2)
+    .fontSize('12')
+    .font('Times-Bold')
+    .text('Vulnerabilities Detected', {
+      align: 'left'
+    })
+    .moveDown(0.5)
+
+  vulnerabilities.forEach((vulnerability) => {
+    doc
+      .fontSize('10')
+      .font('Times-Bold')
+      .fillColor('black')
+      .text(vulnerability.name, {
+        underline: true,
+        align: 'left'
+      })
+      .font('Times-Roman')
+      .text('Severity Level: ', {
+        continued: true
+      })
+
+    if (vulnerability.severity === 'LOW') {
+      doc
+        .font('Times-Bold')
+        .fillColor('#fbda19')
+        .text(vulnerability.severity, {
+          continued: false
+        })
+    } else if (vulnerability.severity === 'MEDIUM') {
+      doc
+        .font('Times-Bold')
+        .fillColor('#ff9600')
+        .text(vulnerability.severity, {
+          continued: false
+        })
+    } else if (vulnerability.severity === 'HIGH') {
+      doc
+        .font('Times-Bold')
+        .fillColor('#f44336')
+        .text(vulnerability.severity, {
+          continued: false
+        })
+    }
+
+    if (vulnerability.count === 1) {
+      doc
+        .font('Times-Roman')
+        .fillColor('black')
+        .text('Found: ' + vulnerability.count + ' Instance')
+    } else {
+      doc
+        .font('Times-Roman')
+        .fillColor('black')
+        .text('Found: ' + vulnerability.count + ' Instances')
+    }
+
+    let instanceCount = 0
+    vulnerability.instances.forEach((instance) => {
+      instanceCount += 1
+      doc
+        .fillColor('black')
+        .text('Instance Number ' + instanceCount + ':')
+        .fillColor('#3d9edf')
+        .text(JSON.stringify(instance))
+        .moveDown(0.5)
+    })
+
+    doc
+      .moveDown(0.5)
+      .fillColor('#8585f8')
+      .font('Times-Bold')
+      .text('Suggested Mitigations:')
+
+    let mitigationCount = 0
+    vulnerability.mitigations.forEach((mitigation) => {
+      mitigationCount += 1
+      doc
+        .font('Times-Roman')
+        .text(mitigationCount + ': ' + mitigation)
+    })
+    doc.moveDown(2)
+  })
+
+  // Finalize the PDF and save to file path
+  doc.end()
+  return `${filePath}/smart-vds-report.pdf`
+}
+
+module.exports = { downloadReport }
